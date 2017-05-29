ed259a55d179e5f926e18c24f7a3e61b70f43844
cc <1186669460@qq.com>
2017-05-29 21:35:12 +0800

    Initial commit

---
+++ app/src/main/java/com/cheng/cc/customcontrols/BaseActivity.java
@@ -0,0 +1,50 @@
+package com.cheng.cc.customcontrols;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ *Activity基类
+ */
+public abstract class BaseActivity extends FragmentActivity{
+    public Bundle savedInstanceState;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.savedInstanceState = savedInstanceState;
+        super.onCreate ( savedInstanceState );
+        initView ();
+        initInstall();
+        initData();
+    
+
+    }
+
+ 
+
+    /**
+     * 初始化布局
+     */
+    abstract public void initView();
+
+    /***
+     * 初始化设置
+     */
+    abstract public void initInstall();
+
+
+    /***
+     * 初始化数据
+     */
+    abstract public void initData();
+
+
+    /***
+     * 事件控制
+     */
+    abstract public void evenControl();
+
+
+
+}
---
+++ app/src/main/java/com/cheng/cc/customcontrols/MainActivity.java
@@ -0,0 +1,33 @@
+package com.cheng.cc.customcontrols;
+
+/**
+ * 控件选择页
+ */
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    public void initView() {
+        AppActivityManager.getAppManager().addActivity(this);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void initInstall() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void evenControl() {
+
+    }
+
+
+
+
+}
