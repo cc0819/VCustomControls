1836fca5236fed7b432d7d65d2ea05b646234b5d
cc <1186669460@qq.com>
2017-05-30 18:03:36 +0800

    增加相应控件分类说明

--- app/src/main/java/com/cheng/cc/customcontrols/MainActivity.java
+++ app/src/main/java/com/cheng/cc/customcontrols/MainActivity.java
@@ -1,5 +1,9 @@
 package com.cheng.cc.customcontrols;
 
+import android.view.View;
+
+import butterknife.OnClick;
+
 /**
  * 控件选择页
  */
@@ -8,8 +12,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        AppActivityManager.getAppManager().addActivity(this);
         setContentView(R.layout.activity_main);
+        AppActivityManager.getAppManager().addActivity(this);
     }
 
     @Override
@@ -27,7 +31,20 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    @OnClick({R.id.btn_textview, R.id.btn_editview, R.id.btn_buttonview, R.id.btn_progressview, R.id.btn_otherview})
+    public void onClickView(View v) {
 
+        switch (v.getId()) {
+            case R.id.btn_textview:
+                break;
+            case R.id.btn_editview:
+                break;
+            default:
+                break;
+        }
+
+
+    }
 
 
 }
